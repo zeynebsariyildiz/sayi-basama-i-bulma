f60f90bdf4b0e05615b126a05c91e6c05d5c16dc
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-30 18:49:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sayi_basamaği_bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir Sayi Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sayac = 0;
+            while (sayi > 0)
+            {
+                sayi /= 10;
+                sayac++;
+            }
+            Console.WriteLine("Girdiginiz Sayı " + sayac + " Basamaklıdır.");
+
+            Console.ReadLine();
+        }
+    }
+}
